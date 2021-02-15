49ba811aedfa66d604c29aba663f8140b77acf01
La-da <ladakohoutova@gmail.com>
2021-02-15 23:30:27 +0900

    initial

---
+++ exp_code/pacman_pilot_main.m
@@ -0,0 +1,14 @@
+%% PaCMan experiment - main script
+
+%% 1. get basedir/set path + create subject folder + input subject ID + input session no.
+% 'lk_mac' -- DB mac, 'lk_git_mac -- git mac'
+
+[basedir, session_no, sid, subject_dir] = pacman_directory_sub_info('lk_git_mac');
+
+%% 2. generate low level category order for the whole session - input: session no.
+
+low_level_order = pacman_pilot_gen_lowlevel_order(session_no);
+
+%% 3. Core function for each run - inputs: basedir + subject ID + output of 2. + others
+ 
+pacman_pilot_run_core(basedir, subject_dir, sid, session_no, low_level_order, 'testmode')
